b5ca4ba577e8947c35c20fa67d168f1eda3bbafd
Alexa Hoynacke <111096384+ahoynacke@users.noreply.github.com>
2023-01-05 17:20:58 -0500

    Delete PCA_Health_indicators.R

--- PCA_Health_indicators.R
+++
@@ -1,9 +0,0 @@
-health_indicators<-read.csv("/Users/alexahoynacke/Desktop/CWRUBootcamp/Final Project/2011_Updated.csv")
-dim(health_indicators)
-
-health_indicators = data.frame(health_indicators)
-
-numerical_features = health_indicators[,c("HTM4","WTKG3","X_BMI5","X_FRUTSUM","X_VEGESUM")]
-
-pca_result = prcomp(numerical_features, scale = TRUE)
-dim(pca_results$x)
